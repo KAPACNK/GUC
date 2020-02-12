25d167198b8f1fa7c75bd228969a0e3ec086baea
kapacuk <kapa.cuk@ya.ru>
2020-02-12 14:25:33 +0300

    done

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php 
-echo "string";
+<?php
+function GetValueByKey($fileName ,$key){
+	//Открываем поток
+	$handle = @fopen($fileName, "r");
+	if ($handle) {
+		//Построчно читаем из файлового указателя
+	    while (($buffer = fgets($handle)) !== false) {
+	    	//Ищим совпадение по регулярке
+	    	if(preg_match('/('.$key.')\t(.*)\n/' ,$buffer, $match) != 0){
+	    		//Если значение найдено, возвращаем
+	    		return ($match[2]);
+	    	}
+	    }
+	    //Если значение не найдено, возвращаем undefined
+	    echo 'Не найдено значение по ключу '.$key;
+	    return "undefined";
+	    fclose($handle);
+	}else{
+		//Еслм не удалось открыть файловый поток, возвращаем undefined
+		echo 'Не удалось открыть файл';
+		return "undefined";
+	}
+}
+
+//Проверка
+$value = GetValueByKey('test.txt', "key___4");
+echo("<br>Значение: " . $value);
+echo "<br>";
+echo "Памяти использовано:".memory_get_usage(true);
